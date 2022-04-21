5a25c558162d4f67831e7d880bb9cb34a55c88c1
Ahmed Elsayed <ahmels@gmail.com>
2022-04-21 21:08:36 +0200

    Add project files.

---
+++ testnative/ImGui.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+
+namespace Mochi.DearImGui;
+
+partial class ImGui
+{
+    public unsafe static void CHECKVERSION()
+        => DebugCheckVersionAndDataLayout(IMGUI_VERSION, (uint)sizeof(ImGuiIO), (uint)sizeof(ImGuiStyle), (uint)sizeof(Vector2), (uint)sizeof(Vector4), (uint)sizeof(ImDrawVert), sizeof(ushort));
+}
